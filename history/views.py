f40f1ab063c8706156b775b177d234405f3a989d
Seo jihee <84059211+bbyu2@users.noreply.github.com>
2022-11-17 15:17:32 +0900

    please

--- history/views.py
+++ history/views.py
@@ -1,8 +1,32 @@
+import json
+
 from django.shortcuts import render
 
+import pyupbit
+import jwt
+import hashlib
+import os
+import requests
+import uuid
+from urllib.parse import urlencode, unquote
 # Create your views here.
 from django.http import HttpResponse
 
+
 def main_index(request):
-    return render(request, 'history/index.html')
+    access = "zNoKwaKvzaxoQl9c8AuVSOoKAXPAK4dG3LdCvPvP"  # 본인 값으로 변경
+    secret = "Ew8TtCScDYGV5QTOexlhTeoptob0RSCixm32iTr9"  # 본인 값으로 변경
+    upbit = pyupbit.Upbit(access, secret)
+    krw = upbit.get_balance()
+    my_balance = upbit.get_balances()
+
+    # print(my_balance[1]['currency'])
+
+    for i in my_balance:
+         print(i['currency'], i['balance'])
+
+    # print(coin[1]['currency'])
+
+    return render(request, 'history/index.html', {'my_balance':my_balance,'krw':krw})
+
     # return HttpResponse("장고 잔고조회, 거래내역 조회 앱 입니다.")
--- users/models.py
+++ users/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
+
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, id, name, email, sex, addr, access_key, secret_key, password=None):
+        if not id:
+            raise ValueError('user_id error')
+
+        user = self.model(
+            id=id,
+            name=name,
+            email=email,
+            sex=sex,
+            addr=addr,
+            access_key=access_key,
+            secret_key=secret_key
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, id, name, email, sex, addr, access_key, secret_key, password):
+        user = self.create_user(
+            id=id,
+            password=password,
+            name=name,
+            email=email,
+            sex=sex,
+            addr=addr,
+            access_key=access_key,
+            secret_key=secret_key
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    id = models.CharField(
+        verbose_name='아이디',
+        max_length=32,
+        unique=True,
+        primary_key=True,
+        null=False
+    )
+    password = models.CharField(max_length=128, null=False)
+    date_of_birth = models.DateField(null=True)
+    name = models.CharField(max_length=16, null=False)
+    email = models.EmailField(max_length=128, unique=True, null=False)
+    sex = models.CharField(max_length=10, null=False)
+    addr = models.CharField(max_length=300, null=False)
+    register_dttm = models.DateTimeField(auto_now_add=True)
+    access_key = models.CharField(unique=True, null=True, max_length=200)
+    secret_key = models.CharField(unique=True, null=True, max_length=200)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'id'
+    REQUIRED_FIELDS = ['name', 'email', 'sex', 'addr', 'password', 'access_key', 'secret_key']
+
+    def __str__(self):
+        return self.id
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
