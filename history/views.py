c9db6275b308337731cf29ba5e06d87f003b89c9
dongwoo <dnehdajrwk11@gmail.com>
2022-11-18 15:44:04 +0900

    잔고 조회 1차 완성

--- history/views.py
+++ history/views.py
@@ -1,5 +1,4 @@
 import json
-
 from django.shortcuts import render
 
 import pyupbit
@@ -17,16 +16,11 @@ def main_index(request):
     access = "zNoKwaKvzaxoQl9c8AuVSOoKAXPAK4dG3LdCvPvP"  # 본인 값으로 변경
     secret = "Ew8TtCScDYGV5QTOexlhTeoptob0RSCixm32iTr9"  # 본인 값으로 변경
     upbit = pyupbit.Upbit(access, secret)
+
     krw = upbit.get_balance()
     my_balance = upbit.get_balances()
-
-    # print(my_balance[1]['currency'])
-
-    for i in my_balance:
-         print(i['currency'], i['balance'])
-
-    # print(coin[1]['currency'])
-
-    return render(request, 'history/index.html', {'my_balance':my_balance,'krw':krw})
+    # print(my_balance)
+    print(upbit.get_order(["KRW-DOGE", "KRW-BTC"], state="done"))
+    return render(request, 'history/index.html', {'my_balance': my_balance, 'krw': krw})
 
     # return HttpResponse("장고 잔고조회, 거래내역 조회 앱 입니다.")
