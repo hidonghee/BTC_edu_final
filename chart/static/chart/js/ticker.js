504031053d497127a9c03bfafbbd46d2fa01b88e
donghee shin <tlsehdgml159@gmail.com>
2022-11-29 16:04:04 +0900

    donghee develop

--- chart/static/chart/js/ticker.js
+++ chart/static/chart/js/ticker.js
@@ -1,10 +1,11 @@
 var wsUri = "wss://api.upbit.com/websocket/v1";
 
-var strong;
+// var strong;
 
 //  현재가 init
 function init() {
-    strong = document.getElementById("coin_strong");
+    // strong = document.getElementById("coin_strong");
+
     testWebSocket();
 }
 
@@ -48,7 +49,16 @@ function onMessage(evt) {
     var arr = new Uint8Array(evt.data);
     //console.log(enc.decode(arr));
     var data = JSON.parse(enc.decode(arr));
-    writeToScreen('<span style="color: blue;">' + data.tp + '</span>',data.cd+'_price');
+
+    // color select
+    var color;
+    if (data.c == 'FALL'){color = '#1261c4'}
+    else if (data.c == 'RISE'){color = '#c84a31'}
+    else if(data.c == 'EVEN') {color = '#4f555a'}
+    writeToScreen('<span style=color:'+ color +'>' + data.tp + '</span>', data.cd+'_price');
+    if (document.getElementById(data.cd+'_main_price')!= null){
+        eventwriteToScreen('<span style=color:'+ color +'>' + data.tp + '</span>',data.cd+'_main_price');
+    }
 }
 
 function onError(evt) {
@@ -66,9 +76,15 @@ function test_writeToScreen(message) {
     pre.innerHTML = message;
 }
 
+// message 인자는 응답.
 function writeToScreen(message, htmlid) {
     var pre = document.getElementById(htmlid);
     pre.innerHTML = message;
+
+}
+function eventwriteToScreen(message, htmlid){
+    var main_price2 = document.getElementById(htmlid);
+    main_price2.innerHTML = message;
 }
 
 window.addEventListener("load", init, false);
