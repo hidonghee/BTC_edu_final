91e7f961815a6d743e020df5a1131b29d341205d
Seo jihee <84059211+bbyu2@users.noreply.github.com>
2023-01-17 13:50:02 +0900

    Update views.py

--- autotrading/views.py
+++ autotrading/views.py
@@ -41,10 +41,10 @@ abc_secret = ""
 def inserkey(request):
 
 
-    ip = "abctest.cluster-czgliwfs2orh.ap-northeast-2.rds.amazonaws.com"
-    dbname = "abcbit"
-    username = "master"
-    passwd = "qwer1234"
+    ip = "###"
+    dbname = "###"
+    username = "###"
+    passwd = "###"
     login_id = request.user
     try:    
         conn = pymysql.connect(host=ip, user=username, password=passwd, db=dbname, use_unicode=True, charset='utf8')
