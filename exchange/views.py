7f259eaf2d7792843ce96559151d94aee7020cee
Seo jihee <84059211+bbyu2@users.noreply.github.com>
2023-01-17 13:58:53 +0900

    Update views.py

--- exchange/views.py
+++ exchange/views.py
@@ -5,8 +5,8 @@ from history.views import abc_access, abc_secret, getdata
 # Create your views here.
 from django.http import HttpResponse
 
-access = "zNoKwaKvzaxoQl9c8AuVSOoKAXPAK4dG3LdCvPvP"  # 본인 값으로 변경
-secret = "Ew8TtCScDYGV5QTOexlhTeoptob0RSCixm32iTr9"  # 본인 값으로 변경
+access = "###"  # 본인 값으로 변경
+secret = "###"  # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
 
